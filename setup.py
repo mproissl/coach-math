c66c7cec101bc9b49c0868926833a09f67ee7bdd
Manuel Proissl <mproissl@gmail.com>
2022-08-29 18:03:13 +0200

    Add legacy cfg

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='coach-math'
+    version='0.0.1',
+    author='Manuel Proissl',
+    description='A simple Python-based coach to solve problems in mathematics.',
+    url='https://github.com/mproissl/coach-math',
+    python_requires='>=3.6',
+    install_requires=['pyyaml', 'googletrans'],
+)
